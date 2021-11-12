3bf4d18492b55afca1ce51a3276f72fe5ddea282
nicobartok0 <nicobartok0@gmail.com>
2021-11-12 12:23:59 -0300

    12/11

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,11 +1,11 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_pymongo import PyMongo, ObjectId
 from flask_cors import CORS
 
 app = Flask(__name__)
 app.config['MONGO_URI']='mongodb://localhost/pythonreactdb'
 mongo = PyMongo(app)
-
+CORS(app)
 db = mongo.db.users
 
 @app.route('/users', methods=['POST'])
@@ -15,24 +15,45 @@ def createUser():
         "email": request.json["email"],
         "password": request.json["password"]
     })
-    print(str(ObjectId(id)))
-    return 'recibido'
+    return jsonify(str(ObjectId(id)))
 
 @app.route('/users', methods=['GET'])
 def getUsers():
-    return 'recibido'
+    users = []
+    for doc in db.find():
+        users.append({
+            '_id': str(ObjectId(doc['_id'])),
+            'name': doc['name'],
+            'email': doc['email'],
+            'password': doc['password']
+        })
+    return jsonify(users)
 
-@app.route('/user/', methods=['GET'])
-def getUser():
-    return 'recibido'
+@app.route('/user/<id>', methods=['GET'])
+def getUser(id):
+    print(id)
+    user = db.find_one({'_id': ObjectId(id)})
+    print(user)
+    return jsonify({
+        '_id': str(ObjectId(user['_id'])),
+        'name': user['name'],
+        'email': user['email'],
+        'password': user['password']
+    })
 
-@app.route('/users/<id>', methods=['GET'])
-def deleteUser():
-    return 'recibido'
+@app.route('/users/<id>', methods=['DELETE'])
+def deleteUser(id):
+    db.delete_one({'_id': ObjectId(id)})
+    return jsonify({'msg': 'Usuario Eliminado'})
 
 @app.route('/users/<id>', methods=['PUT'])
-def updateUser():
-    return 'recibido'
+def updateUser(id):
+    db.update_one({'_id': ObjectId(id)}, {'$set': {
+        'name': request.json['name'],
+        'email': request.json['email'],
+        'password': request.json['password'] 
+    }})
+    return jsonify({'msg': 'Usuario Actualizado'})
 
 if __name__ == '__main__':
     app.run(debug=True)
